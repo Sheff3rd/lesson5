dbbf02621ab15ff90a3d9a002dd7be5803913a5a
Sheff3rd <thedoctorwhoovesmail@gmail.com>
2016-11-16 03:14:05 +0200

    Registration, user shown if logged in, log in, log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'log_in' => 'sessions#new', as: :log_in
+  get 'log_out' => 'sessions#destroy', as: :log_out
+
+  get "sign_up" => "users#new", as: :sign_up
+
+  resources :users
+  resources :sessions
+
   root to: 'tasks#index'
   resources :tasks, except: [:index, :show] do
     patch '/', action: :update_all, on: :collection
